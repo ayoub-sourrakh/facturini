66076be56939595f380e1df4e2fb52d5483f5905
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2026-06-11 05:11:09 +0200

    fix: use async adapters for queue and cable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,7 @@ Rails.application.configure do
 
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  config.active_job.queue_adapter = :async
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
