c389c76159c77b77651368da4a814cecad4ed473
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2026-06-11 23:27:25 +0200

    feat: maintenance mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :check_maintenance_mode
+
+  private
+
+  def check_maintenance_mode
+    if ENV["MAINTENANCE_MODE"] == "true"
+      render file: Rails.public_path.join("maintenance.html"), layout: false, status: :service_unavailable
+    end
+  end
 end
